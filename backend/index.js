62da2ec1fbdfd641ab77760a96c292a80d7059ae
I4MGlitch <cassamedicalbali@gmail.com>
2025-11-21 18:55:24 +0800

    2

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,8 @@ const app = express();
 app.use(cors());
 app.use(compression());
 app.use(parser.json({ limit: '50mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const mongoURI = 'mongodb+srv://cassamedicalbali:CassaClinic@cassamedicalclinic.369z5aw.mongodb.net/CassaMedicalClinic';
 
